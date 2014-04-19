96da656b4f67658acc111b96b476dc53bc34fa42
Nick Quinlan <nicholas.quinlan@sendgrid.com>
2014-04-19 09:56:10 -0700

    WIP

---
+++ configure/applications.sh
@@ -0,0 +1,21 @@
+# Casks
+brew cask install google-chrome
+
+brew cask install sublime-text
+
+brew cask install quicksilver
+defaults write com.blacktree.Quicksilver QSShowMenuIcon 0
+defaults write com.blacktree.Quicksilver "Hide Dock Icon" 0 
+defaults write com.blacktree.Quicksilver QSCommandInterfaceControllers QSPrimerInterfaceController 
+
+brew cask install colorpicker-hex
+
+brew cask install github
+
+brew cask install spotify
+
+#brew cask install calq
+#defaults write com.katoemba.calq "Activation Modifiers" 4352
+#defaults write com.katoemba.calq "Hide Delay" 10
+#defaults write com.katoemba.calq "Clear On Hide" Yes
+#defaults write com.katoemba.calq "Hide On Copy" Yes
---
+++ configure/binaries.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if test ! $(which brew)
+then
+  echo "  Installing Homebrew for you."
+  ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)" > /tmp/homebrew-install.log
+  brew doctor
+fi
+
+curl https://raw.github.com/creationix/nvm/v0.4.0/install.sh | sh
+
+
+brew install
+
+brew tap phinze/cask
+brew install brew-cask
