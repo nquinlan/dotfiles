2173972660bf0a21e2a60895dfcba72e39ef9aef
Nick Quinlan <nicholas.quinlan@sendgrid.com>
2014-06-25 13:40:43 -0600

    added todo, fixed bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,5 +2,8 @@
 
 # Get things running!
 for file in ~/.dotfiles/{zshrc,vimrc,gitconfig}; do
-	ln -fs "~/.dotfiles/$file" "~/.$file"
+	ln -fs ~/.dotfiles/$file ~/.$file
 done
+
+ln -fs ~/.dotfiles/todo ~/todo/todo.cfg
+ln -fs ~/.dotfiles/todo ~/.todo/config
