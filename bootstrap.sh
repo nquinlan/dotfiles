253da10b44e1e4b70dfdeb67f5d5159d427bcb23
Nick Quinlan <nicholas.quinlan@sendgrid.com>
2014-11-05 11:09:21 -0500

    fixed todo errors

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,5 +5,9 @@ for file in {zshrc,bash_profile,vimrc,gitconfig}; do
 	ln -fs ~/.dotfiles/$file ~/.$file
 done
 
+for file in ~/{todo,.todo} do
+	mkdir --parents $file
+done
+
 ln -fs ~/.dotfiles/todo ~/todo/todo.cfg
 ln -fs ~/.dotfiles/todo ~/.todo/config
