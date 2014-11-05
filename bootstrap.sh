c99504f553087502975ee82cc036854c412e6cf0
Nick Quinlan <nicholas.quinlan@sendgrid.com>
2014-11-05 11:52:55 -0500

    added concession for bash_rc

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,8 @@ for file in {zshrc,bash_profile,vimrc,gitconfig}; do
 	ln -fs ~/.dotfiles/$file ~/.$file
 done
 
+ln -fs ~/.dotfiles/bash_profile ~/.bashrc
+
 for file in ~/{todo,.todo} do
 	mkdir --parents $file
 done
